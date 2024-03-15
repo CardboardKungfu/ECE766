6fd494d68b9400d04af52a567e371c22c0bf204a
Jed Pulley <jedediah.pulley@gmail.com>
2024-03-15 15:56:32 -0500

    HW5.1a complete

--- HW5/Python/hw5_challenge1.py
+++ HW5/Python/hw5_challenge1.py
@@ -9,7 +9,27 @@ def findSphere(img: np.ndarray) -> Tuple[np.ndarray, float]:
     # Output:
     #   center - 2x1 vector of the center of the sphere
     #   radius - radius of the sphere
-    raise NotImplementedError
+    from skimage import filters, measure
+
+    # Binarize the image using Otsu's thresholding method
+    threshold_value = filters.threshold_otsu(img)
+    binary_img = img > threshold_value
+    
+    # Find contours in the binary image
+    contours = measure.find_contours(binary_img, 0.5)
+    
+    # Assuming the largest contour corresponds to the sphere
+    largest_contour = max(contours, key=len)
+    
+    # Compute the centroid of the largest contour
+    centroid = np.mean(largest_contour, axis=0).astype(np.int)
+    
+    # Compute the radius of the sphere from its area
+    area = measure.regionprops(binary_img.astype(int))[0].area
+    radius = np.sqrt(area / np.pi)
+    
+    # raise NotImplementedError
+    return centroid, radius
 
 def computeLightDirections(center: np.ndarray, radius: float, images: List[np.ndarray]) -> np.ndarray:
     # Compute the light source directions
