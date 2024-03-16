5c8f1829dc8bb79db04f6766ba898d9cf96c65c0
Jed Pulley <jedediah.pulley@gmail.com>
2024-03-16 14:56:46 -0500

    HW5: Housekeeping

--- HW5/Python/hw5_challenge1.py
+++ HW5/Python/hw5_challenge1.py
@@ -15,10 +15,8 @@ def findSphere(img: np.ndarray) -> Tuple[np.ndarray, float]:
     threshold_value = filters.threshold_otsu(img)
     binary_img = img > threshold_value
     
-    # Find contours in the binary image
+    # Find contours in the binary image, assuming the largest contour corresponds to the sphere
     contours = measure.find_contours(binary_img, 0.5)
-    
-    # Assuming the largest contour corresponds to the sphere
     largest_contour = max(contours, key=len)
     
     # Compute the centroid of the largest contour
@@ -51,13 +49,6 @@ def computeLightDirections(center: np.ndarray, radius: float, images: List[np.nd
         x = (x_pix - center[1]) / radius
         y = (y_pix - center[0]) / radius
         z = np.sqrt(1 - x**2 - y**2)  # Assuming sphere is centered at (0, 0, 0)
-        
-        # vec = np.array([x_pix, y_pix])
-        # vec -= center
-        # z = np.sqrt(radius**2 - np.dot(vec, vec))
-        # vec_3D = np.append(vec, z)
-        # norm_vec = vec_3D / np.linalg.norm(vec_3D)
-        # light_dirs_5x3.append(norm_vec)
 
         # # Normalize the vector to get the normal direction
         normal_vector = np.array([x, y, z])
@@ -91,13 +82,6 @@ def computeNormals(light_dirs: np.ndarray, images: List[np.ndarray], mask: np.nd
     # Output:
     #   normals - HxWx3 matrix of surface normals
     #   albedo_img - HxW matrix of albedo values
-    
-    # light_dirs = np.array([[-0.07626101, -0.00851292, 0.99705155],
-    #                         [-0.45957245, 0.19101906, 0.86735511],
-    #                         [-0.3020472, -0.39182436, 0.86904612],
-    #                         [ 0.30179958, -0.23429911, 0.92413253],
-    #                         [ 0.14427434, 0.40105272, 0.90462237]
-    #                         ])
 
     height, width = mask.shape
     normals = np.zeros((height, width, 3), dtype=np.float64)
@@ -105,23 +89,15 @@ def computeNormals(light_dirs: np.ndarray, images: List[np.ndarray], mask: np.nd
 
     stacked_imgs = np.stack(images, axis=-1)
 
-    normals[..., 2] = mask
-
     for row in range(height):
         for col in range(width):
             if mask[row, col]: # use mask to tell if point is foreground
-                # I = np.sort(stacked_imgs[row, col])[::-1]
                 I = stacked_imgs[row, col, :].astype(np.float64)
-                # N = np.linalg.pinv(light_dirs) @ I
-                N = np.linalg.lstsq(light_dirs.T @ light_dirs, light_dirs.T @ I, rcond=None)[0]
+                N = np.linalg.pinv(light_dirs) @ I
                 albedo = np.linalg.norm(N)
                 normal = N / albedo
                 normals[row, col] = normal
                 albedo_img[row, col] = albedo
 
-    # albedo_max = np.max(albedo_img)
-    # albedo_img /= albedo_max
-
+    # raise NotImplementedError
     return normals, albedo_img
-
-    raise NotImplementedError
