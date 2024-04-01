2257ea0f8c792c564e045b1b3b5800009d774b26
Jed Pulley <jedediah.pulley@gmail.com>
2024-04-01 13:24:35 -0500

    Challenge1a partial

--- HW6/Python/hw6_challenge1.py
+++ HW6/Python/hw6_challenge1.py
@@ -2,6 +2,8 @@ from PIL import Image
 import numpy as np
 from typing import Union, Tuple, List
 
+from scipy import ndimage
+
 def generateIndexMap(gray_list: List[np.ndarray], w_size: int) -> np.ndarray:
     # Generate an index map for the refocusing application
     # Input:
@@ -11,7 +13,31 @@ def generateIndexMap(gray_list: List[np.ndarray], w_size: int) -> np.ndarray:
     #   index_map - mxn index map
     #               index_map(i, j) is the index of the image that is in focus
     #               at pixel (i, j)
-    raise NotImplementedError
+    
+    height, width = gray_list[0].shape
+    index_map = np.empty((height, width))
+    gray_stack = np.stack(gray_list, axis=2)
+
+    # for i, j in np.ndindex((height, width)):
+    for j in range(width):
+        for i in range(height):
+            # Clip window so it doesn't fall out of bounds
+            left = np.clip(i - w_size, 0, width - 1)
+            right = np.clip(i + w_size, 0, width - 1)
+            top = np.clip(j - w_size, 0, height - 1)
+            bottom = np.clip(j + w_size, 0, height - 1)
+            
+            # Check if window size is non-zero
+            if left < right and top < bottom:
+                # Slice the ixj column of the 3d stack
+                window_column = gray_stack[left:right, top:bottom, :]
+                
+                # Find the index of the maximum laplacian value over each of the layers in our window column
+                # index_map[i, j] = np.argmax(np.apply_over_axes(ndimage.laplace, window_column, [0,1]))
+                index_map[i, j] = np.argmax(np.apply_along_axis(ndimage.laplace, axis=2, arr=window_column))
+
+    # raise NotImplementedError
+    return index_map
 
 
 def loadFocalStack(focal_stack_dir: str) -> Tuple[List[np.ndarray], List[np.ndarray]]:
@@ -21,7 +47,17 @@ def loadFocalStack(focal_stack_dir: str) -> Tuple[List[np.ndarray], List[np.ndar
     # Output:
     #   rgb_list - List of RGB images for varying focal lengths
     #   gray_list - List of gray-scale images for varying focal lengths
-    raise NotImplementedError
+    
+    import glob
+    rgb_list = []
+    gray_list = []
+    for filename in glob.glob(focal_stack_dir + '/*.jpg'): #assuming gif
+        img = Image.open(filename)
+        rgb_list.append(np.array(img))
+        gray_list.append(np.array(img.convert('L')))
+    
+    # raise NotImplementedError
+    return (rgb_list, gray_list)
 
 
 def refocusApp(rgb_list: List[np.ndarray], depth_map: np.ndarray) -> None:
--- HW6/Python/runHw6.py
+++ HW6/Python/runHw6.py
@@ -61,7 +61,7 @@ def challenge1a():
     # gray_stack is an mxnxk matrix.
 
     # Specify the (half) window size used for focus measure computation
-    # half_window_size = ?? 
+    half_window_size = 25
 
     # Generate an index map, here we will only use the gray-scale images
     index_map = generateIndexMap(gray_stack, half_window_size)
