cc144ee8d46c326167f0465b230f44afec220996
Jed Pulley <jedediah.pulley@gmail.com>
2024-04-12 15:21:30 -0500

    Update edge detection

--- final-project/stop-sign-masker/masker.py
+++ final-project/stop-sign-masker/masker.py
@@ -16,14 +16,14 @@ def filter_img(path):
 
 def edge_detection(filtered_img):    
     median = cv2.medianBlur(filtered_img, 5)
+    closed = cv2.morphologyEx(median, cv2.MORPH_CLOSE, (3, 3))
+    closed = cv2.morphologyEx(median, cv2.MORPH_CLOSE, (5, 5))
     closed = cv2.morphologyEx(median, cv2.MORPH_CLOSE, (7, 7))
-
-    sobelxy = cv2.Sobel(src=closed, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
-    return sobelxy
+    return cv2.Canny(closed, 50, 150)  # Using Canny edge detection
 
 # filtered_image = cv2.resize(filtered_image, (0, 0), fx = 0.5, fy = 0.5)
 
-paths = ["stop1.jpg", "stop2.jpg", "stop3.jpg", "organic-stop.jpeg"]
+paths = ["stop1.jpg", "stop2.jpg", "stop3.jpg", "stop4.jpg", "stop5.jpg"]
 
 for path in paths:
     # Display the filtered RGB image
@@ -31,7 +31,7 @@ for path in paths:
     cv2.imshow('Filtered Image', filtered)
 
     sobel = edge_detection(filtered)
-    cv2.imshow('Sobel Image', sobel)
+    cv2.imshow('Canny Image', sobel)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
