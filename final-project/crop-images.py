7a8ae7d860852a05633eaa57e9a67f3a8e4241ad
Jed Pulley <jedediah.pulley@gmail.com>
2024-04-12 17:25:23 -0500

    use bbox instead of segmentation

--- final-project/crop-images.py
+++ final-project/crop-images.py
@@ -10,22 +10,31 @@ with open(coco_json_path, "r") as file:
 images = coco_data["images"]
 annotations = coco_data["annotations"]
 
+count = 0
 for i, annotation in enumerate(annotations):
+    if count > 250:
+        break
+
+    count += 1
     for j, img in enumerate(images):
         if annotation["image_id"] == img["id"]:
             filename = img["file_name"]
             height = img["height"]
             width = img["width"]
             bbox = annotation["bbox"]
-            segmentation = annotation["segmentation"]
+            # segmentation = annotation["segmentation"]
             img_PIL = Image.open("dataset\\" + filename)
             background = Image.new("RGB", (width, height), color="black")
 
             # Create a mask for the object
             mask = Image.new("L", (width, height), 0)
             draw = ImageDraw.Draw(mask)
-            for segment in segmentation:
-                draw.polygon(segment, fill=255)
+            # for segment in segmentation:
+            #     draw.polygon(segment, fill=255)
+            x1, y1, width, height = bbox
+            x2 = x1 + width
+            y2 = y1 + height
+            draw.rectangle([x1, y1, x2, y2], fill=255) # draw bounding box, not octagon
             
             padded_mask = np.array(mask)
             padded_mask = ndimage.binary_dilation(padded_mask, iterations=5)
