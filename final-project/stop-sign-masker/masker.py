4bd90dd8cf430126139162b79969901c8c36c70b
Jed Pulley <jedediah.pulley@gmail.com>
2024-04-12 14:31:17 -0500

    add for loop

--- final-project/stop-sign-masker/masker.py
+++ final-project/stop-sign-masker/masker.py
@@ -1,24 +1,23 @@
 import cv2
 import numpy as np
 
-# Load RGB image
-rgb_image = cv2.imread('stop3.jpg')  
 
-# Convert RGB image to HSV
-hsv_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
+def filter_img(path):    
+    # Load RGB image
+    rgb_image = cv2.imread(path)  
 
-# Filter HSV image based on conditions
-min_hue = 0        # Minimum hue (red)
-max_hue = 10       # Maximum hue (red)
-min_sat = 100      # Minimum saturation
-max_sat = 255      # Maximum saturation
-min_val = 100      # Minimum value (brightness)
-max_val = 255      # Maximum value (brightness)
+    # Convert RGB image to HSV
+    hsv_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
 
-filtered_image = cv2.inRange(hsv_image, (min_hue, min_sat, min_val), (max_hue, max_sat, max_val))
+    # Filter image based on min HSV and max HSV values
+    return cv2.inRange(hsv_image, (0, 100, 0), (10, 255, 255))
+
 # filtered_image = cv2.resize(filtered_image, (0, 0), fx = 0.5, fy = 0.5)
 
-# Display the filtered RGB image
-cv2.imshow('Filtered Image', filtered_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+paths = ["stop1.jpg", "stop2.jpg", "stop3.jpg", "organic-stop.jpeg"]
+
+for path in paths:
+    # Display the filtered RGB image
+    cv2.imshow('Filtered Image', filter_img(path))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
