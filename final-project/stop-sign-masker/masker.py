05cfa2bb4e641784591f2d0a7ee3687126f1dfa1
Jed Pulley <jedediah.pulley@gmail.com>
2024-04-12 14:23:40 -0500

    housekeeping

--- final-project/stop-sign-masker/masker.py
+++ final-project/stop-sign-masker/masker.py
@@ -1,17 +1,11 @@
 import cv2
 import numpy as np
 
-def rgb_to_sbh(rgb_image):
-    # Convert RGB image to HSV
-    hsv_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
-
-    return hsv_image
-
 # Load RGB image
 rgb_image = cv2.imread('stop3.jpg')  
 
 # Convert RGB image to HSV
-hsv_image = rgb_to_sbh(rgb_image)
+hsv_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
 
 # Filter HSV image based on conditions
 min_hue = 0        # Minimum hue (red)
@@ -23,8 +17,6 @@ max_val = 255      # Maximum value (brightness)
 
 filtered_image = cv2.inRange(hsv_image, (min_hue, min_sat, min_val), (max_hue, max_sat, max_val))
 # filtered_image = cv2.resize(filtered_image, (0, 0), fx = 0.5, fy = 0.5)
-# # Convert filtered HSV image to RGB
-# filtered_rgb_image = cv2.cvtColor(filtered_image, cv2.COLOR_HSV2RGB)
 
 # Display the filtered RGB image
 cv2.imshow('Filtered Image', filtered_image)
