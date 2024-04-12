011e30249c9f706f3c135323e0dfd73856aafb61
Jed Pulley <jedediah.pulley@gmail.com>
2024-04-12 14:49:22 -0500

    Add sobel

--- final-project/stop-sign-masker/masker.py
+++ final-project/stop-sign-masker/masker.py
@@ -10,7 +10,16 @@ def filter_img(path):
     hsv_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
 
     # Filter image based on min HSV and max HSV values
-    return cv2.inRange(hsv_image, (0, 100, 0), (10, 255, 255))
+    lower_thresh = np.array([0,100,50])
+    upper_thresh = np.array([10,255,255])
+    return cv2.inRange(hsv_image, lower_thresh, upper_thresh)
+
+def edge_detection(filtered_img):    
+    median = cv2.medianBlur(filtered_img, 5)
+    closed = cv2.morphologyEx(median, cv2.MORPH_CLOSE, (7, 7))
+
+    sobelxy = cv2.Sobel(src=closed, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
+    return sobelxy
 
 # filtered_image = cv2.resize(filtered_image, (0, 0), fx = 0.5, fy = 0.5)
 
@@ -18,6 +27,11 @@ paths = ["stop1.jpg", "stop2.jpg", "stop3.jpg", "organic-stop.jpeg"]
 
 for path in paths:
     # Display the filtered RGB image
-    cv2.imshow('Filtered Image', filter_img(path))
+    filtered = filter_img(path)
+    cv2.imshow('Filtered Image', filtered)
+
+    Sobel = edge_detection(filtered)
+    cv2.imshow('Sobel Image', Sobel)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
